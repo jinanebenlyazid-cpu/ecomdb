3e3100bbabe9c460d2fb7706edcab96e74bbf62f
Jinane <jinanebenliazid@gmail.com>
2026-01-13 15:48:01 +0100

    configuration vercel

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; // ✅ DERNIÈRE MODIFICATION ICI
 
     /**
      * The headers that should be used to detect proxies.
